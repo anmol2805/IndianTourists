502cf479552c8cbca812efed8c0421e0292d6d73
anmol2805 <saxenaanmol28@gmail.com>
2017-07-22 22:48:32 +0530

    initial commit

---
+++ app/src/main/java/com/anmol/indiantourists/MainActivity.java
@@ -0,0 +1,170 @@
+package com.anmol.indiantourists;
+
+import android.*;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.menu.ListMenuItemView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SectionIndexer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    Boolean check = false;
+    ListView list;
+    ArrayList<String> citylist;
+    ArrayAdapter<String> cityadapter;
+    private String [] permissions = {android.Manifest.permission.RECORD_AUDIO,
+            android.Manifest.permission.INTERNET,
+            android.Manifest.permission.ACCESS_NETWORK_STATE,
+            android.Manifest.permission.CHANGE_NETWORK_STATE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_CALENDAR,
+            android.Manifest.permission.WRITE_CALENDAR,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.CALL_PHONE,
+            android.Manifest.permission.READ_PHONE_STATE,
+            android.Manifest.permission.MODIFY_PHONE_STATE,
+            android.Manifest.permission.CAPTURE_AUDIO_OUTPUT,
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.READ_CONTACTS,
+            android.Manifest.permission.WRITE_CONTACTS
+
+    };
+    private int permissionRecord;
+    private DatabaseReference databaseReference;
+    Button addcity;
+    String cityname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, permissions, 100);
+        list = (ListView)findViewById(R.id.list);
+        addcity = (Button)findViewById(R.id.addcity);
+        citylist = new ArrayList<String>();
+        cityadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,citylist);
+        list.setAdapter(cityadapter);
+        databaseReference = FirebaseDatabase.getInstance().getReference().getRoot();
+        addcity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showalertdialog();
+            }
+        });
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+                Set<String> set = new HashSet<String>();
+                while (iterator.hasNext()){
+                    set.add((String)((DataSnapshot)iterator.next()).getKey());
+                }
+                citylist.clear();
+                citylist.addAll(set);
+                cityadapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = ((TextView)view).getText().toString();
+                Bundle b = new Bundle();
+                b.putString("city",s);
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void showalertdialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter City Name");
+        final EditText edittext = new EditText(this);
+        builder.setView(edittext);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cityname = edittext.getText().toString();
+                if(!TextUtils.isEmpty(cityname)){
+
+                    for(int i = 0;i<list.getAdapter().getCount();i++){
+                        if(cityname.equals((String)list.getAdapter().getItem(i))){
+                            check = true;
+                        }
+                    }
+                    if(check.equals(true)){
+                        Toast.makeText(MainActivity.this,"City already exists",Toast.LENGTH_SHORT).show();
+                        check = false;
+                    }else {
+                        Map<String,Object> map = new HashMap<String, Object>();
+                        map.put(cityname,"");
+                        databaseReference.updateChildren(map);
+                    }
+                }
+
+                else {
+                    showalertdialog();
+                    Toast.makeText(MainActivity.this,"Please Enter a Valid City Name",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }).setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 10){
+            permissionRecord = grantResults[0] = PackageManager.PERMISSION_GRANTED;
+            int length = grantResults.length;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/anmol/indiantourists/SecondActivity.java
@@ -0,0 +1,134 @@
+package com.anmol.indiantourists;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class SecondActivity extends AppCompatActivity {
+    Boolean check = false;
+    Button addplace;
+    ListView place;
+    ArrayList<String> placelist;
+    ArrayAdapter<String> placeadapter;
+    DatabaseReference placedatabase;
+    String city;
+    String placename;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        city = getIntent().getExtras().getString("city");
+        setTitle(city);
+        place = (ListView)findViewById(R.id.list);
+        addplace = (Button)findViewById(R.id.addplace); 
+        placelist = new ArrayList<String>();
+        placeadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,placelist);
+        place.setAdapter(placeadapter);
+        addplace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showaddplace();
+            }
+        });
+        placedatabase = FirebaseDatabase.getInstance().getReference().getRoot().child(city);
+        placedatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+                Set<String> set = new HashSet<String>();
+                while (iterator.hasNext()){
+                    set.add((String)((DataSnapshot)iterator.next()).getKey());
+                }
+                placelist.clear();
+                placelist.addAll(set);
+                placeadapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        place.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = ((TextView)view).getText().toString();
+                Bundle b = new Bundle();
+                b.putString("place",s);
+                b.putString("city",city);
+                Intent intent = new Intent(SecondActivity.this,FirstActivity.class);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showaddplace() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Place Name");
+        final EditText edittext = new EditText(this);
+        builder.setView(edittext);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                placename = edittext.getText().toString();
+                if(!TextUtils.isEmpty(placename)){
+
+                    for(int i = 0;i<place.getAdapter().getCount();i++){
+                        if(placename.equals((String)place.getAdapter().getItem(i))){
+                            check = true;
+                        }
+                    }
+                    if(check.equals(true)){
+                        Toast.makeText(SecondActivity.this,"Place already exists",Toast.LENGTH_SHORT).show();
+                        check = false;
+                    }else {
+                        Map<String,Object> map = new HashMap<String, Object>();
+                        map.put(placename,"");
+                        placedatabase.updateChildren(map);
+                    }
+                }
+
+                else {
+                    showaddplace();
+                    Toast.makeText(SecondActivity.this,"Please Enter a Valid Place Name",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }).setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+}
